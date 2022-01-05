24097672c097940b2da08068bf2046e90841e7b3
selimgr <jaawis.dev@gmail.com>
2022-01-05 03:27:07 +0100

    ajout -x elf_read et changements args

--- elf_read/elf_read.c
+++ elf_read/elf_read.c
@@ -14,54 +14,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "elf_lib/elf_lib.h"
+#include "elf_lib/elf_utils.h"
 
 void usage(char *name) {
 	fprintf(stderr, "Usage:\n"
-		"%s [ -H | --help ] [ -h | -S | -r ] file\n\n"
+		"%s [ -H | --help ] [ -h | -S | -x | -r ] file\n\n"
 		"Display information about the contents of ELF format files\n"
 		, name);
 }
+
+int sectionsAAfficher_s = 0;
+char *sectionsAAfficher[100];
 
 int main(int argc, char *argv[]) {
     int opt;
 
     /* à compléter */
 	struct option longopts[] = {
-		{ "header", required_argument, NULL, 'h' },
-		{ "sections", required_argument, NULL, 'S' },
-		{ "help", no_argument, NULL, 'H' },
+		{ "header", no_argument, NULL, 'h' },
+		{ "sections", no_argument, NULL, 'S' },
+		{ "section", required_argument, NULL, 'x' },
 		{ "relocations", required_argument, NULL, 'r' },
+		{ "help", no_argument, NULL, 'H' },
 		{ NULL, 0, NULL, 0 }
 	};
 
+    char *ELF_filename;
+    int showHeader = 0, showSectionsH = 0;
+
     /* à compléter */
-    while ((opt = getopt_long(argc, argv, "h:S:r:H", longopts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "hSx:r:H", longopts, NULL)) != -1) {
 		switch(opt) {
             case 'h': {
-                /** ici on devrait afficher le header du fichier ELF donné en argument **/
-                printf("Fichier: %s\n", optarg);
-                FILE *f = fopen(optarg, "rb");
-
-                if (f == NULL) exit(1);
-
-                Elf32 header;
-                init_header(f, &header);
-                write_elf(stdout, header);
+                showHeader = 1;
                 break;
             }
             case 'S': {
-                /** ici on devrait afficher les sections du fichier ELF donné en argument **/
-                printf("Fichier: %s\n", optarg);
-                FILE *f = fopen(optarg, "rb");
-
-                if (f == NULL) exit(1);
-
-                Elf32 header;
-                Elf32_SH sections[SH_TABLE_MAX];
-
-                init_header(f, &header);
-                read_sections(f, header, sections);
-                print_sections_header(f, header, sections);
+                showSectionsH = 1;
+                break;
+            }
+            case 'x': {
+                if (sectionsAAfficher_s < 100) {
+                    sectionsAAfficher[sectionsAAfficher_s] = optarg;
+                    sectionsAAfficher_s++;
+                }
                 break;
             }
             case 'r':
@@ -78,5 +74,53 @@ int main(int argc, char *argv[]) {
                 exit(1);
 		}
 	}
+
+    if (argc-optind <= 0) {
+        // Si aucun fichier fourni
+        exit(1);
+    } else {
+        ELF_filename = argv[optind];
+        FILE *f = fopen(ELF_filename, "rb");
+
+        if (f == NULL) exit(1);
+
+        Elf32 header;
+        Elf32_SH sections[SH_TABLE_MAX];
+        init_header(f, &header);
+        read_sections(f, header, sections);
+
+        if (showHeader) {
+            write_elf(stdout, header);
+        }
+
+        if (showSectionsH) {
+            print_sections_header(f, stdout, header, sections);
+        }
+
+        if (sectionsAAfficher_s > 0) {
+            for (int i = 0; i < sectionsAAfficher_s; i++) {
+                char *nom_sec = sectionsAAfficher[i];
+                int num = 0;
+                int result = sscanf(nom_sec, "%d", &num);
+                if (result == 1) {
+                    if (num >= 0 && num < header.e_shnum) print_data_section(f, stdout, header, sections, &sections[num]);
+                    else printf("-- No section number %d.\n", num);
+                } else {
+                    // si chaîne de caractères, on cherche l'index de la section nom_sec
+                    // dans le tableau
+                    int found = 0;
+                    for (int j = 0; j < header.e_shnum; j++) {
+                        if (strcmp(read_name_from_STable(f, header, sections[header.e_shstrndx], sections[j].sh_name), nom_sec) == 0) {
+                            found = 1;
+                            break;
+                        }
+                    }
+                    if (found == 1) print_data_section(f, stdout, header, sections, &sections[found]);
+                    else printf("-- No section named '%s' found.\n", nom_sec);
+
+                }
+            }
+        }
+    }
 
 }
